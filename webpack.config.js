7b3387b49a87ed9cbb39cc53fecf85a481fe9f6c
Darylxyx <921218060@qq.com>
2018-03-15 17:27:26 +0800

    更新构建工具

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
 var webpack = require('webpack'),
 	path = require('path');
 
+function resolve(dir) {
+	return path.join(__dirname, dir);
+}
+
 module.exports = {
 	entry: ['babel-polyfill', './src/index.js'],
 	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js'
+		path: resolve('dist'),
+		filename: 'bundle.js',
+	},
+	resolve: {
+		alias: {
+			'@': resolve('src'),
+		},
 	},
 	module: {
 		loaders: [
-			{
-				test: /\.css$/,
-				loader: 'style-loader!css-loader'
-			},
 			{
 				test: /\.js(x)*$/,
 				loader: 'babel-loader',
@@ -27,8 +32,12 @@ module.exports = {
                 exclude: /node_modules/
 			},
 			{
-				test: /\.(png|jpg)$/,
-				loader: 'url-loader?limit=8192&name=images/[hash:8].[name].[ext]'
+				test: /\.(png|jpe?g|gif|svg)$/,
+				loader: 'url-loader',
+				options: {
+					limit: 10000,
+					name: 'img/[name].[hash:7].text',
+				},
 			}
 		]
 	},
