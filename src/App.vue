409d7ea576e5004dbecf347c15eb6e6da9662e4f
xieyuxiao <xieyuxiao@g7.com.cn>
2018-03-22 18:17:58 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-    <img src="@/assets/img/logo.png">
+  <div id='app'>
+    <img src='@/assets/img/logo.png'>
     <router-view/>
   </div>
 </template>
@@ -11,7 +11,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang='less' scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div class='hello'>
     <h1>{{ msg }}</h1>
   </div>
 </template>
@@ -15,20 +15,10 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang='less' scoped>
+.hello {
+  h1, h2 {
+    font-weight: normal;
+  }
 }
 </style>
