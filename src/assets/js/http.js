29c005f877139f21e55b891298554b056942b31a
xieyuxiao <xieyuxiao@g7.com.cn>
2018-04-08 13:56:20 +0800

    update

--- src/assets/js/http.js
+++ src/assets/js/http.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import qs form 'qs';
+import Cookies from 'js-cookie';
 
 window.axios = axios;
 
@@ -20,5 +22,50 @@ const apiConfig = {
 const ENV = apiConfig[window.location.host];
 
 export default {
-	install(Vue) {}
+	install(Vue) {
+		const baseUrl = `${ENV.url}`;
+
+		const fetch = axios.create({
+			transformRequest: [data => qs.stringify(data)],
+		});
+
+		fetch.interceptors.request.use((config) => {
+			if (!Cookies.get('_TOKEN')) {
+				//未登陆时的操作
+			}
+			return config;
+		}, error => Promise.reject(error));
+
+		fetch.interceptors.response.use((response) => {
+			if (response.data.code !== 0) {
+				//提示接口业务错误
+				return Promise.reject(new Error(response.data.msg));
+			}
+			return response.data.data;
+		}, (error) => {
+			let message = error.message;
+
+			// 统一提示接口错误
+			return Promise.reject(error);
+		});
+
+		window.$http = {
+			get(path, params = {}) {
+				const url = baseUrl + path;
+				return fetch({
+					method: 'get',
+					url,
+					params,
+				});
+			},
+			post(path, data = {}) {
+				const url = baseUrl + path;
+				return fetch({
+					method: 'post',
+					url,
+					data,
+				});
+			}
+		};
+	},
 };
