a3d79d18ef3a3889c685cbdc4604ab401b67c016
Darylxyx <921218060@qq.com>
2018-04-19 15:23:25 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,8 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     //关闭语句强制分号结尾
-    'semi': [0]
+    'semi': [0],
+    //关闭禁用tab
+    "no-tabs": "off"
   }
 }
